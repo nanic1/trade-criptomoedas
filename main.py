8ce65e58c2939fdeb4b91d7551148740f5d9a0a0
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2025-02-26 13:54:57 -0300

    consultando quantas criptomoedas na carteira eu tenho

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 from binance.client import Client
 from password import api_key, api_secret
 
-client = Client(api_key, api_secret)
-
-status = client.get_account_status()
-print(status)
+client = Client(api_key, api_secret, testnet =True)
 
 # COMANDOS IMPORTANTES!!! 'buyerCommission' 'canTrade' 'canWithdraw' 'canDeposit' 'uid' 'balances' 'accountType
 
 # puxar info da conta do usuario utilizando o bot
 info = client.get_account()
-for i in info:
-    print(i)
 
 # puxar saldo de TODAS as moedas
 lista_ativos = info["balances"]
 
-# puxar saldos de TODOS os ativos do usuario
-for ativo in lista_ativos:
-    if float(ativo["free"]) > 0:
-        print(ativo)
+# seleciona a moeda desejada
+criptomoeda = input('Digite o código da moeda que deseja tradar (ex: BTC, ETH, etc): ').upper()
+encontrado = False
 
-# venda
-from binance.enums import *
-order = client.create.order(
-    symbol='BNBBTC',
-    side=SIDE_BUY,
-    type=ORDER_TYPE_MARKET,
-    timeInForce=TIME_IN_FORCE_GTC,
-    quantity=100,
-    price='0.00001'
-)
+# puxa o saldo do ativo seleciodado pelo usuario
+for ativo in lista_ativos:
+    if ativo['asset'] == criptomoeda:
+        print(f'{criptomoeda} = {ativo['free']} {criptomoeda}')
+        encontrado = True
+        break
 
 # mostra se o cliente pode tradar ou não
-# print(info["canTrade"])
+print(info["canTrade"])
