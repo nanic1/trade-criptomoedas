beaef0a1c8037fa0d46000819a1d6b758c46d676
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2025-03-20 21:35:09 -0300

    pagina mostrando moedas em tempo real

--- start.py
+++ start.py
@@ -1,15 +1,41 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, render_template_string, request, redirect, session
+from flask_socketio import SocketIO
+from binance.client import Client
+from password import api_key, api_secret
+import threading
+import time
 
 app = Flask(__name__)
 
+client = Client(api_key, api_secret)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
 user = 'admin'
 password = 'senhasegura123'
+app.secret_key = 'Batat@12!'
+symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'XRPUSDT']
 
-@app.route('/login')
+@app.route('/')
 def login():
     return render_template ('login.html')
 
-@app.route('/valid_login', methods=['POST',])
+def fetch_prices():
+    while True:
+        prices = {}
+        for symbol in symbols:
+            try:
+                ticker = client.get_symbol_ticker(symbol=symbol)
+                prices[symbol] = float(ticker['price'])
+            except Exception as e:
+                prices[symbol] = f"Erro: {str(e)}"
+        socketio.emit('update_prices', prices)
+        time.sleep(1)  # Atualiza a cada 2 segundos
+
+@app.route('/mainPage')
+def main_page():
+    return render_template('mainpage.html')
+
+@app.route('/validate', methods=['POST',])
 def valid_login():
     if request.form['password'] == password  and request.form['username'] == user:
 
@@ -19,4 +45,7 @@ def valid_login():
     else:
         return redirect('/login')
     
-app.run(debug=True)
+
+if __name__ == '__main__':
+    threading.Thread(target=fetch_prices, daemon=True).start()
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
